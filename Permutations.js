2b7d36677df869e3d5ab4d8c1bfa4a3f1a7720d2
Yin <67538811+YinHk@users.noreply.github.com>
2021-09-13 22:24:47 +0800

    Update Permutations.js

--- Permutations.js
+++ Permutations.js
@@ -32,3 +32,56 @@ var permute = function(nums) {
     }
     return matrix
 };
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    let matrix = []
+    let index = 0
+    let list = nums
+    
+    const swap = (index) => {
+           
+           for(let i=index;i<=nums.length-1;i++) {
+                
+                let temp
+                temp = list[index]
+                list[index] = list[i+1]
+                list[i+1] = list[index] 
+                matrix.push(list)
+            }
+           index++
+           if(index<nums.length-1) { 
+             swap(index)
+           }
+    }
+    return matrix
+};
+
+/**
+1234    2134    3214    4231
+
+1[234]:  1234 1324 1432
+````     12[34]:  1234  1243
+         13[24]:  1324  1342
+         14[32]:  1432  1423
+
+2[134]:  2134 2314 2431
+         21[34]:  2134  2143
+         23[14]:  2314  2341
+         24[31]:  2431  2413
+
+3[214]:  3214 3124 3412
+         32[14]:  3214  3241
+         31[24]:  3124  3142
+         34[12]:  3412  3421
+
+4[231]:  4231 4321 4132
+         42[31]:  4231  4213
+         43[21]:  4321  4312
+         41[32]:  4132  4123
+**/
+
