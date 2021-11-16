07ac80c993254b680166619b930ec998783402b4
Yin <67538811+YinHk@users.noreply.github.com>
2021-11-16 23:15:46 +0800

    Update Letter Combinations of a Phone Number.js

--- Letter Combinations of a Phone Number.js
+++ Letter Combinations of a Phone Number.js
@@ -5,7 +5,7 @@
  */
 var letterCombinations = function(digits) {
    
-  const phoneNums = { 2:'abc', 3:'def',4:'ghi',5:'jkl',6:'mno',7:'pqrs',8:'tuv',9:'wxyz'}
+  const phoneNums = { 2:'abc', 3:'def',4:'ghi',5:'jkl',6:'mno',7:'pqrs',8:'tuv',9:'wxyz' }
   let arr = []
 
   for(let i=0;i<=digits.length-1;i++) {
