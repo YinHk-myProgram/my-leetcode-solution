878e7f7859bff39927b0b20dcba9bb9ec7d04638
Yin <67538811+YinHk@users.noreply.github.com>
2021-12-22 13:36:40 +0800

    Create Intersection of Two Arrays.java

---
+++ 349. Intersection of Two Arrays/Intersection of Two Arrays.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    
+    ArrayList<Integer> arr = new ArrayList<Integer>();
+    public boolean linearSearch(int[] a, int val) {
+      for(int element:a) {
+        if (element == val) {
+            return true;
+        }  
+      } 
+      return false;
+    }
+    
+    public int[] intersection(int[] nums1, int[] nums2) {
+       for(int i:nums1) { //System.out.println(Arrays.asList(nums2).contains(i));
+          if(linearSearch(nums2,i)){
+            if(!arr.contains(i)){
+                arr.add(i); 
+            }
+          } 
+       }
+       int[] intersection = new int[arr.size()];
+       for(int i=0;i<intersection.length;i++){ 
+          intersection[i] = arr.get(i).intValue();  //convert arrlist to primitive int array
+       }
+       return intersection;
+    }
+    
+}
