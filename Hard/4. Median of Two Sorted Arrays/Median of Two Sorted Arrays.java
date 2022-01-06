bd7a58d161361f8a73f32369859028c7c0f7771f
Yin <67538811+YinHk@users.noreply.github.com>
2022-01-06 11:13:44 +0800

    Create Median of Two Sorted Arrays.java

---
+++ Hard/4. Median of Two Sorted Arrays/Median of Two Sorted Arrays.java
@@ -0,0 +1,37 @@
+/*
+   Convert array to array list
+   merge and then sort them
+   find the median
+ */
+
+class Solution {
+    
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        
+        List<Integer> list = new ArrayList<Integer>();
+        List<Integer> alist1 = Arrays.stream(nums1).boxed().collect(Collectors.toList());
+        List<Integer> alist2 = Arrays.stream(nums2).boxed().collect(Collectors.toList());
+        list.addAll(alist1);
+        list.addAll(alist2);
+        Collections.sort(list);
+        //System.out.println(list);
+       
+        
+        int index;
+        
+        if(list.size()%2==0) {
+            index =  list.size()/2;
+            double median = ((double) list.get(index-1) + (double) list.get(index))/2;
+            return median;
+        }else {
+            index = (list.size()+1)/2;
+            double median = (double) list.get(index-1);
+            return median;
+        }
+        
+        
+        
+    }
+    
+}
+
