bbe56f9d11435f243eaaaf778a504b403c7108c3
Ken <67538811+YinHk@users.noreply.github.com>
2022-06-03 16:46:16 -0400

    Create generateParentheses.py

---
+++ Medium/22. Generate Parentheses/generateParentheses.py
@@ -0,0 +1,19 @@
+def generate(result: List[str], s: str, _open: int, close: int, n: int):
+    # Base condition
+    if _open == n and close == n:
+        result.append(s)
+        return
+    # If the number of _open parentheses is less than the given n
+    if _open < n:
+        generate(result, s + "(", _open + 1, close, n)
+    # If we need more close parentheses to balance
+    if close < _open:
+        generate(result, s + ")", _open, close + 1, n)
+
+
+def generateParenthesis(n: int) -> List[str]:
+    # Resultant list
+    result = []
+    # Recursively generate parentheses
+    generate(result, "", 0, 0, n)
+    return result
